05c9087053e7786703f9009a75293dcceea81c5a
Matt Hidinger <matt.hidinger@gmail.com>
2019-06-07 13:44:28 -0700

    fixes

--- src/FindTemplate/index.ts
+++ src/FindTemplate/index.ts
@@ -2,27 +2,31 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, templateFieldsBlob: any): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    
+
 
 
     let dataProperties = Object.getOwnPropertyNames(req.body);
     let final = [];
-    for(let template of templateFieldsBlob) {
+    for (let template of templateFieldsBlob) {
         let match = {
-            file: template.templatePath,
-            propMatchCount: 0
+            templateUrl: template.templatePath,
+            propMatchCount: 0,
+            confidence: 0.0
         };
 
-        for(let dataProp of dataProperties) { 
-            for(let templateProp of template.properties) {
-                if(dataProp === templateProp) {
+        for (let dataProp of dataProperties) {
+            for (let templateProp of template.properties) {
+                if (dataProp === templateProp) {
                     match.propMatchCount++;
                 }
             }
         }
 
-        if(match.propMatchCount > 0) 
+        if (match.propMatchCount > 0) {
+            match.confidence = match.propMatchCount / dataProperties.length;
+            delete match.propMatchCount;
             final.push(match);
+        }
     }
 
     // sort descending
