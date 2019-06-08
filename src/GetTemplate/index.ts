a6fb90e6fb3172c3eb64bfcdca1b5db2f50a90a2
Matt Hidinger <matt.hidinger@gmail.com>
2019-06-07 21:36:18 -0700

    adding list function

--- src/GetTemplate/index.ts
+++ src/GetTemplate/index.ts
@@ -4,23 +4,23 @@ import * as ACData from "adaptivecards-templating"
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, templateBlob: any): Promise<void> {
     context.log('HTTP trigger function processed a request.');
 
-    if(templateBlob) {
+    if (templateBlob) {
 
         let body: string;
-        
-        if(req.method === 'POST') {  // Populate the template using the POST body
+
+        if (req.method === 'POST') {  // Populate the template using the POST body
             var template = new ACData.Template(templateBlob);
 
             var dataContext = new ACData.EvaluationContext();
             dataContext.$root = req.body;
-            
+
             body = template.expand(dataContext);
         }
         else { //  return the raw template
             body = templateBlob
         }
 
-        if(req.query.sampleData) {
+        if (req.query.sampleData) {
             //body["$sampleData"] = { "x": "y"}
         } else {
             delete body["$sampleData"];
@@ -30,14 +30,14 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             body: body,
             headers: { 'Content-Type': 'application/json' }
         };
-        
-       
+
+
     } else {
         context.res = {
             status: 404,
             body: "No template found for " + req.params.name
         };
-    }    
+    }
 };
 
 export default httpTrigger;
--- src/ListTemplates/index.ts
+++ src/ListTemplates/index.ts
@@ -3,7 +3,8 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, templateBlob: any): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     context.res = {
-        body: templateBlob
+        body: templateBlob,
+        headers: { 'Content-Type': 'application/json' }
     };
 };
 
