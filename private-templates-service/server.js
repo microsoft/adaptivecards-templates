92fa71e3507f3ae34ca5e81ffacbfc01cc6a2d44
Kody Yang <kodyyyang@gmail.com>
2020-01-23 13:09:15 -0800

    init server and client

---
+++ private-templates-service/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get("/express_backend", (req, res) => {
+	res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+});
