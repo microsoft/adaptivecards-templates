1015f6ae80b447fe7216302bc701ca6cfe389e7d
Kody Yang <kodyyyang@gmail.com>
2020-01-23 11:08:03 -0800

    Update app ID

--- private-templates-service/client/src/Config.js
+++ private-templates-service/client/src/Config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  appId: '54ac897b-9063-4599-8169-350528af2fd9',
+  appId: '4803f66a-136d-4155-a51e-6d98400d5506',
   redirectUri: 'https://adaptivecms.z22.web.core.windows.net/',
   scopes: [
     'user.read',
