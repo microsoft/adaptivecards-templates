41dd76bf68616c60cb5f08e964f3d3ee197420a1
Kody Yang <kodyyyang@gmail.com>
2020-01-23 17:15:32 -0800

    update backend to serve static frontend

--- private-templates-service/server.js
+++ private-templates-service/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+app.use(express.static(path.join(__dirname, "./client/build")));
 const port = process.env.PORT || 5000;
 
 // console.log that your server is up and running
@@ -7,5 +9,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get("/express_backend", (req, res) => {
-	res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+	res.send({
+		express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT. CONFIRMED"
+	});
 });
