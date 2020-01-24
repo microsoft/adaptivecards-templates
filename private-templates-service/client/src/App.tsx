47b398ec7153676328f973506e5bc2a4fadbfa3c
Kody Yang <kodyyyang@gmail.com>
2020-01-24 15:27:31 -0800

    whitespace

--- private-templates-service/client/src/App.tsx
+++ private-templates-service/client/src/App.tsx
@@ -10,196 +10,196 @@ import { getUserDetails, getOrgDetails } from "./GraphService";
 import "bootstrap/dist/css/bootstrap.css";
 
 interface State {
-	isAuthenticated: boolean;
-	user: object;
-	error: any;
+  isAuthenticated: boolean;
+  user: object;
+  error: any;
 }
 
 class App extends Component<any, State> {
-	userAgentApplication: UserAgentApplication;
+  userAgentApplication: UserAgentApplication;
 
-	constructor(props: any) {
-		super(props);
+  constructor(props: any) {
+    super(props);
 
-		console.log(JSON.stringify(props));
+    console.log(JSON.stringify(props));
 
-		this.userAgentApplication = new UserAgentApplication({
-			auth: {
-				clientId: config.appId,
-				redirectUri: config.redirectUri
-			},
-			cache: {
-				cacheLocation: "localStorage",
-				storeAuthStateInCookie: true
-			}
-		});
+    this.userAgentApplication = new UserAgentApplication({
+      auth: {
+        clientId: config.appId,
+        redirectUri: config.redirectUri
+      },
+      cache: {
+        cacheLocation: "localStorage",
+        storeAuthStateInCookie: true
+      }
+    });
 
-		var user = this.userAgentApplication.getAccount();
+    var user = this.userAgentApplication.getAccount();
 
-		this.state = {
-			isAuthenticated: user !== null,
-			user: {},
-			error: null
-		};
+    this.state = {
+      isAuthenticated: user !== null,
+      user: {},
+      error: null
+    };
 
-		if (user) {
-			// Enhance user object with data from Graph
-			this.getUserProfile();
-		}
-	}
+    if (user) {
+      // Enhance user object with data from Graph
+      this.getUserProfile();
+    }
+  }
 
-	render() {
-		let error = null;
-		if (this.state.error) {
-			error = (
-				<ErrorMessage
-					message={this.state.error.message}
-					debug={this.state.error.debug}
-				/>
-			);
-		}
+  render() {
+    let error = null;
+    if (this.state.error) {
+      error = (
+        <ErrorMessage
+          message={this.state.error.message}
+          debug={this.state.error.debug}
+        />
+      );
+    }
 
-		return (
-			<Router>
-				<div>
-					<NavBar
-						isAuthenticated={this.state.isAuthenticated}
-						authButtonMethod={
-							this.state.isAuthenticated
-								? this.logout.bind(this)
-								: this.login.bind(this)
-						}
-						user={this.state.user}
-					/>{" "}
-					<Container>
-						{" "}
-						{error}{" "}
-						<Route
-							exact
-							path="/"
-							render={props => (
-								<Welcome
-									{...props}
-									isAuthenticated={this.state.isAuthenticated}
-									user={this.state.user}
-									authButtonMethod={this.login.bind(this)}
-								/>
-							)}
-						/>{" "}
-					</Container>{" "}
-				</div>{" "}
-			</Router>
-		);
-	}
+    return (
+      <Router>
+        <div>
+          <NavBar
+            isAuthenticated={this.state.isAuthenticated}
+            authButtonMethod={
+              this.state.isAuthenticated
+                ? this.logout.bind(this)
+                : this.login.bind(this)
+            }
+            user={this.state.user}
+          />{" "}
+          <Container>
+            {" "}
+            {error}{" "}
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <Welcome
+                  {...props}
+                  isAuthenticated={this.state.isAuthenticated}
+                  user={this.state.user}
+                  authButtonMethod={this.login.bind(this)}
+                />
+              )}
+            />{" "}
+          </Container>{" "}
+        </div>{" "}
+      </Router>
+    );
+  }
 
-	setErrorMessage(message: any, debug: any) {
-		this.setState({
-			error: {
-				message: message,
-				debug: debug
-			}
-		});
-	}
+  setErrorMessage(message: any, debug: any) {
+    this.setState({
+      error: {
+        message: message,
+        debug: debug
+      }
+    });
+  }
 
-	async login() {
-		try {
-			await this.userAgentApplication.loginPopup({
-				scopes: config.scopes,
-				prompt: "select_account"
-			});
-			await this.getUserProfile();
-		} catch (err) {
-			var error = {};
+  async login() {
+    try {
+      await this.userAgentApplication.loginPopup({
+        scopes: config.scopes,
+        prompt: "select_account"
+      });
+      await this.getUserProfile();
+    } catch (err) {
+      var error = {};
 
-			if (typeof err === "string") {
-				var errParts = err.split("|");
-				error =
-					errParts.length > 1
-						? {
-								message: errParts[1],
-								debug: errParts[0]
-						  }
-						: {
-								message: err
-						  };
-			} else {
-				error = {
-					message: err.message,
-					debug: JSON.stringify(err)
-				};
-			}
+      if (typeof err === "string") {
+        var errParts = err.split("|");
+        error =
+          errParts.length > 1
+            ? {
+              message: errParts[1],
+              debug: errParts[0]
+            }
+            : {
+              message: err
+            };
+      } else {
+        error = {
+          message: err.message,
+          debug: JSON.stringify(err)
+        };
+      }
 
-			this.setState({
-				isAuthenticated: false,
-				user: {},
-				error: error
-			});
-		}
-	}
+      this.setState({
+        isAuthenticated: false,
+        user: {},
+        error: error
+      });
+    }
+  }
 
-	logout() {
-		this.userAgentApplication.logout();
-	}
+  logout() {
+    this.userAgentApplication.logout();
+  }
 
-	async getUserProfile() {
-		try {
-			// Get the access token silently
-			// If the cache contains a non-expired token, this function
-			// will just return the cached token. Otherwise, it will
-			// make a request to the Azure OAuth endpoint to get a token
+  async getUserProfile() {
+    try {
+      // Get the access token silently
+      // If the cache contains a non-expired token, this function
+      // will just return the cached token. Otherwise, it will
+      // make a request to the Azure OAuth endpoint to get a token
 
-			var accessToken = await this.userAgentApplication.acquireTokenSilent(
-				{
-					scopes: config.scopes
-				}
-			);
+      var accessToken = await this.userAgentApplication.acquireTokenSilent(
+        {
+          scopes: config.scopes
+        }
+      );
 
-			if (accessToken) {
-				// Get the user's profile from Graph
-				var user = await getUserDetails(accessToken);
-				var org = await getOrgDetails(accessToken);
-				if (org.value.length === 0) {
-					org = null;
-				} else {
-					org = org.value[0].displayName;
-				}
-				this.setState({
-					isAuthenticated: true,
-					user: {
-						displayName: user.displayName,
-						email: user.mail || user.userPrincipalName,
-						organization: org
-					},
-					error: null
-				});
-			}
-		} catch (err) {
-			var error = {};
-			if (typeof err === "string") {
-				var errParts = err.split("|");
-				error =
-					errParts.length > 1
-						? {
-								message: errParts[1],
-								debug: errParts[0]
-						  }
-						: {
-								message: err
-						  };
-			} else {
-				error = {
-					message: err.message,
-					debug: JSON.stringify(err)
-				};
-			}
+      if (accessToken) {
+        // Get the user's profile from Graph
+        var user = await getUserDetails(accessToken);
+        var org = await getOrgDetails(accessToken);
+        if (org.value.length === 0) {
+          org = null;
+        } else {
+          org = org.value[0].displayName;
+        }
+        this.setState({
+          isAuthenticated: true,
+          user: {
+            displayName: user.displayName,
+            email: user.mail || user.userPrincipalName,
+            organization: org
+          },
+          error: null
+        });
+      }
+    } catch (err) {
+      var error = {};
+      if (typeof err === "string") {
+        var errParts = err.split("|");
+        error =
+          errParts.length > 1
+            ? {
+              message: errParts[1],
+              debug: errParts[0]
+            }
+            : {
+              message: err
+            };
+      } else {
+        error = {
+          message: err.message,
+          debug: JSON.stringify(err)
+        };
+      }
 
-			this.setState({
-				isAuthenticated: false,
-				user: {},
-				error: error
-			});
-		}
-	}
+      this.setState({
+        isAuthenticated: false,
+        user: {},
+        error: error
+      });
+    }
+  }
 }
 
 export default App;
--- private-templates-service/client/src/Config.tsx
+++ private-templates-service/client/src/Config.tsx
@@ -1,7 +1,7 @@
-import {} from "react"; // Typescripte requires every file to have an import
+import { } from "react"; // Typescripte requires every file to have an import
 
 export default {
-	appId: "4803f66a-136d-4155-a51e-6d98400d5506",
-	redirectUri: "https://adaptivecms.z22.web.core.windows.net/",
-	scopes: ["user.read"]
+  appId: "4803f66a-136d-4155-a51e-6d98400d5506",
+  redirectUri: "https://adaptivecms.z22.web.core.windows.net/",
+  scopes: ["user.read"]
 };
