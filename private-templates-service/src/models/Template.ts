0072608b3ca1548d48721e3085c6487b18cf3efa
Grace Zhang <t-grzhan@microsoft.com>
2020-01-28 14:06:59 -0800

    changes as per pr and fix spacing

--- private-templates-service/src/models/Template.ts
+++ private-templates-service/src/models/Template.ts
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
+// Temporarily supports only mongodb, will change to use database adapter in the future
 const templateSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    template: JSON,
-    ownerOID: String,
-    isPublished: Boolean,
-    created: {
-        type: Date,
-        default: Date.now
-    }
-})
+  _id: mongoose.Schema.Types.ObjectId,
+  template: JSON,
+  ownerOID: String,
+  isPublished: Boolean,
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-export const Template = mongoose.model('Template', templateSchema);
+export const Template = mongoose.model("Template", templateSchema);
