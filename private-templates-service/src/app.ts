ece1bd40fecb9d255225424f414134d568736b8c
Grace Zhang <t-grzhan@microsoft.com>
2020-01-28 14:09:14 -0800

    fix spacing

--- private-templates-service/src/app.ts
+++ private-templates-service/src/app.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import path from 'path';
-import mongoose from 'mongoose';
-import passport from './config/passport';
-import bodyParser from 'body-parser';
-import mongo from 'connect-mongo';
-import session from 'express-session';
-import bluebird from 'bluebird';
-import logger from './util/logger';
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import passport from "./config/passport";
+import bodyParser from "body-parser";
+import mongo from "connect-mongo";
+import session from "express-session";
+import bluebird from "bluebird";
+import logger from "./util/logger";
 
 // import controllers
-import templateRouter from './controllers/template';
+import templateRouter from "./controllers/template";
 
 const app = express();
 
@@ -17,33 +17,36 @@ const app = express();
 const MongoStore = mongo(session);
 mongoose.Promise = bluebird;
 
-mongoose.connect('mongodb://localhost/card',{
-    useNewUrlParser: true, 
-    useCreateIndex: true, 
+mongoose
+  .connect("mongodb://localhost/card", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true
-}).then(
-    () => {},
-).catch(err => {
+  })
+  .then(() => {})
+  .catch(err => {
     logger.error("Mongodb connection error " + err);
-});
+  });
 
 // Express configuration
 app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(session({
+app.use(
+  session({
     resave: true,
     saveUninitialized: true,
     secret: "test",
     store: new MongoStore({
-        url: 'mongodb://localhost/adaptivecard',
-        autoReconnect: true
+      url: "mongodb://localhost/adaptivecard",
+      autoReconnect: true
     })
-}));
+  })
+);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Add routers 
+// Add routers
 app.use("/template", templateRouter);
 
 export default app;
