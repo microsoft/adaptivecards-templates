e9e05bb82508d6b5819a8bb2ce6c2cda1c8c75f8
Danielle Mott <t-damott@microsoft.com>
2020-01-30 10:30:09 -0800

    remove dist folder

--- private-templates-service/server/dist/__tests__/sample.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-describe('Sample Test', () => {
-    it('should test that true === true', () => {
-        expect(true).toBe(true);
-    });
-});
-//# sourceMappingURL=sample.test.js.map
