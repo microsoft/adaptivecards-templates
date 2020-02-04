22c044488dbd53eac64bab70dc85515ed43131cc
nnmkhang <nnmkhang166@gmail.com>
2020-02-04 13:21:26 -0800

    Resolve conflicts

--- private-templates-service/client/src/components/NavBar/styled.tsx
+++ private-templates-service/client/src/components/NavBar/styled.tsx
@@ -1,9 +1,23 @@
 import styled from 'styled-components';
 
-export const AvatarIcon = styled.img`
-  width: 32px;
+export const Banner = styled.div`
+  background-color: #6F6F6F;
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  align-items: center;
+  height:75px;
 `;
 
-export const DefaultAvatarIcon = styled.i`
-  width: 32px;
+export const Styledh1 = styled.h1`
+
+  padding-left:362px;
+  height: 43px;
+  color:#FFFFFF;
+  letter-spacing: 0;
+  opacity:1;
+  font-family: "Segoe UI Semibold";
+  font-size: 32px;
+  margin-bottom:0;
+
 `;
