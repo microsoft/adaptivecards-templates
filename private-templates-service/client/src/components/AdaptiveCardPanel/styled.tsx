9a8e6d3370fdcdd725d9d53180b7e7121d4a15a0
Kody Yang <kodyyyang@gmail.com>
2020-02-05 15:43:37 -0800

    whitespace

--- private-templates-service/client/src/components/AdaptiveCardPanel/styled.tsx
+++ private-templates-service/client/src/components/AdaptiveCardPanel/styled.tsx
@@ -2,21 +2,21 @@ import styled from "styled-components";
 import { COLORS } from '../../globalStyles';
 
 export const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	margin-right: 20px;
+  display: flex;
+  flex-direction: column;
+  margin-right: 20px;
 
-	background: white;
-	border-radius: 5px;
+  background: white;
+  border-radius: 5px;
   width: 350px;
 
   -webkit-box-shadow: 0px 2px 6px 0px rgba(111,111,111,0.4);
   -moz-box-shadow: 0px 2px 6px 0px rgba(111,111,111,0.4);
   box-shadow: 0px 2px 6px 0px rgba(111,111,111,0.4);
 
-	@media only screen and (max-width: 1399px) {
-		margin-bottom: 20px;
-	}
+  @media only screen and (max-width: 1399px) {
+    margin-bottom: 20px;
+  }
 `;
 
 export const ACWrapper = styled.div`
@@ -25,14 +25,14 @@ export const ACWrapper = styled.div`
 `;
 
 export const TemplateName = styled.h2`
-	border-top-style: solid;
-	border-top-color: ${COLORS.BORDER};
-	border-width: 1px;
-	padding: 10px 0 5px 10px;
-	font-size: 0.75rem;
-	color: grey;
+  border-top-style: solid;
+  border-top-color: ${COLORS.BORDER};
+  border-width: 1px;
+  padding: 10px 0 5px 10px;
+  font-size: 0.75rem;
+  color: grey;
 `;
 
 export const Bottom = styled.div`
-	padding: 20px;
+  padding: 20px;
 `;
