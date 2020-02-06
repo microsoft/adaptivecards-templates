bd13bfb2fd300074906709a4893126d5ac1b856c
Kody Yang <kodyyyang@gmail.com>
2020-02-05 16:19:07 -0800

    colors

--- private-templates-service/client/src/components/Common/AdaptiveCard/styled.tsx
+++ private-templates-service/client/src/components/Common/AdaptiveCard/styled.tsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
+import { COLORS } from '../../../globalStyles';
 
 export const Card = styled.div`
 	/* Disables buttons on adaptive card */
 	pointer-events: none;
-	background: #d3d3d3;
-	border-top: 5px;
-	border-top-color: grey;
+	background: ${COLORS.GREY2};
 `;
