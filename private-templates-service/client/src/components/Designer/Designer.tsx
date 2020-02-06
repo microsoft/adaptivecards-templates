e6dcb6cbc71b556540a6737e85eded000bd6c482
guptaharsh25 <me@harshgupta.ca>
2020-02-05 16:44:53 -0800

    kody's changes + changed Designer to functional component to accomodate hooks

--- private-templates-service/client/src/components/Designer/Designer.tsx
+++ private-templates-service/client/src/components/Designer/Designer.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RootState } from '../../store/rootReducer';
 import requireAuthentication from '../../utils/requireAuthentication';
 import { connect } from 'react-redux';
 import { UserType } from '../../store/auth/types';
-
+import { useHistory } from 'react-router-dom';
 //ACDesigner
 import * as monaco from 'monaco-editor';
 import markdownit from 'markdown-it';
@@ -22,48 +22,58 @@ interface DesignerProps {
   authButtonMethod: () => Promise<void>;
 }
 
-class Designer extends React.Component<DesignerProps>{
+const Designer = () => {
 
-  private designer: ACDesigner.CardDesigner | undefined;
+  //useEffect(() => {
+  ACDesigner.GlobalSettings.enableDataBindingSupport = true;
+  ACDesigner.GlobalSettings.showSampleDataEditorToolbox = true;
 
-  componentDidMount(): void {
-    ACDesigner.GlobalSettings.enableDataBindingSupport = true;
-    ACDesigner.GlobalSettings.showSampleDataEditorToolbox = true;
-
-    ACDesigner.CardDesigner.onProcessMarkdown = (text: string, result: { didProcess: boolean, outputHtml?: string }) => {
-      result.outputHtml = new markdownit().render(text);
-      result.didProcess = true;
-    }
-
-    let hostContainers: Array<ACDesigner.HostContainer> = [];
-    hostContainers.push(new ACDesigner.WebChatContainer("Bot Framework WebChat", "containers/webchat-container.css"));
-    hostContainers.push(new ACDesigner.CortanaContainer("Cortana Skills", "containers/cortana-container.css"));
-    hostContainers.push(new ACDesigner.OutlookContainer("Outlook Actionable Messages", "containers/outlook-container.css"));
-    hostContainers.push(new ACDesigner.TimelineContainer("Windows Timeline", "containers/timeline-container.css"));
-    hostContainers.push(new ACDesigner.DarkTeamsContainer("Microsoft Teams - Dark", "containers/teams-container-dark.css"));
-    hostContainers.push(new ACDesigner.LightTeamsContainer("Microsoft Teams - Light", "containers/teams-container-light.css"));
-    hostContainers.push(new ACDesigner.BotFrameworkContainer("Bot Framework Other Channels (Image render)", "containers/bf-image-container.css"));
-    hostContainers.push(new ACDesigner.ToastContainer("Windows Notifications (Preview)", "containers/toast-container.css"));
-
-
-    this.designer = new ACDesigner.CardDesigner(hostContainers);
-    this.designer.sampleCatalogueUrl = window.location.origin;
-    this.designer.assetPath = window.location.origin;
-    const element = document.getElementById("root");
-    if (element) {
-      this.designer.attachTo(element);
-    }
-
-    //Example of how to export data from designer
-    console.log("Adaptive Card Template: " + this.designer.getCard());
-    console.log("Adaptive Card Sample Data: " + this.designer.sampleData);
-
-    this.designer.monacoModuleLoaded(monaco);
+  ACDesigner.CardDesigner.onProcessMarkdown = (text: string, result: { didProcess: boolean, outputHtml?: string }) => {
+    result.outputHtml = new markdownit().render(text);
+    result.didProcess = true;
   }
 
-  render(): React.ReactElement<{}> {
-    return <div id="root" dangerouslySetInnerHTML={{ __html: "dangerouslySetACDesigner" }}></div>
+  let hostContainers: Array<ACDesigner.HostContainer> = initHostContainers();
+
+  let designer = new ACDesigner.CardDesigner(hostContainers);
+  designer.sampleCatalogueUrl = window.location.origin;
+  designer.assetPath = window.location.origin;
+
+  let history = useHistory();
+  let closeButton = new ACDesigner.ToolbarButton("closeButton", "Close", "", (sender) => (history.goBack()));
+
+  closeButton.separator = true;
+  designer.toolbar.insertElementAfter(closeButton, ACDesigner.CardDesigner.ToolbarCommands.TogglePreview);
+
+  const element = document.getElementById("root");
+  if (element) {
+    designer.attachTo(element);
   }
+
+  //Example of how to export data from designer
+  console.log("Adaptive Card Template: " + designer.getCard());
+  console.log("Adaptive Card Sample Data: " + designer.sampleData);
+
+  designer.monacoModuleLoaded(monaco);
+  // }, []);
+
+
+
+  return <div id="root" dangerouslySetInnerHTML={{ __html: "dangerouslySetACDesigner" }}></div>;
 }
 
+function initHostContainers(): Array<ACDesigner.HostContainer> {
+  let hostContainers: Array<ACDesigner.HostContainer> = [];
+
+  hostContainers.push(new ACDesigner.WebChatContainer("Bot Framework WebChat", "containers/webchat-container.css"));
+  hostContainers.push(new ACDesigner.CortanaContainer("Cortana Skills", "containers/cortana-container.css"));
+  hostContainers.push(new ACDesigner.OutlookContainer("Outlook Actionable Messages", "containers/outlook-container.css"));
+  hostContainers.push(new ACDesigner.TimelineContainer("Windows Timeline", "containers/timeline-container.css"));
+  hostContainers.push(new ACDesigner.DarkTeamsContainer("Microsoft Teams - Dark", "containers/teams-container-dark.css"));
+  hostContainers.push(new ACDesigner.LightTeamsContainer("Microsoft Teams - Light", "containers/teams-container-light.css"));
+  hostContainers.push(new ACDesigner.BotFrameworkContainer("Bot Framework Other Channels (Image render)", "containers/bf-image-container.css"));
+  hostContainers.push(new ACDesigner.ToastContainer("Windows Notifications (Preview)", "containers/toast-container.css"));
+
+  return hostContainers;
+}
 export default connect(mapStateToProps)(requireAuthentication(Designer));
