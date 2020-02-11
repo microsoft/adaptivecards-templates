ada79575cce5f26104fb34d4ef731d9d516c3e43
John Travolta <sevkorobot@gmail.com>
2020-02-10 17:50:08 -0800

    Fix spaces issue

--- private-templates-service/adaptivecards-templating-service/src/storageproviders/IStorageProvider.ts
+++ private-templates-service/adaptivecards-templating-service/src/storageproviders/IStorageProvider.ts
@@ -3,18 +3,11 @@ import { JSONResponse, IUser, ITemplate } from "../models/models";
 
 export interface StorageProvider {
   getUsers(query: Partial<IUser>): Promise<JSONResponse<IUser[]>>;
-
   getTemplates(query: Partial<ITemplate>): Promise<JSONResponse<ITemplate[]>>;
-
   updateUser(query: Partial<IUser>, updateQuery: Partial<IUser>): Promise<JSONResponse<Number>>;
-
   updateTemplate(query: Partial<ITemplate>, updateQuery: Partial<ITemplate>): Promise<JSONResponse<Number>>;
-
   insertUser(user: IUser): Promise<JSONResponse<Number>>;
-
   insertTemplate(template: ITemplate): Promise<JSONResponse<Number>>;
-
   removeUser(query: Partial<IUser>): Promise<JSONResponse<Number>>;
-
   removeTemplate(query: Partial<ITemplate>): Promise<JSONResponse<Number>>;
 }
