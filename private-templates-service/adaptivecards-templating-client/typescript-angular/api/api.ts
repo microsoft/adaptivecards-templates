b7cacebfb60c821c63bbf9a18bf0e6da0a840a52
Grace Zhang <t-grzhan@microsoft.com>
2020-02-11 17:40:19 -0800

    add test typescript angular sdk

---
+++ private-templates-service/adaptivecards-templating-client/typescript-angular/api/api.ts
@@ -0,0 +1,5 @@
+export * from './template.service';
+import { TemplateService } from './template.service';
+export * from './user.service';
+import { UserService } from './user.service';
+export const APIS = [TemplateService, UserService];
---
+++ private-templates-service/adaptivecards-templating-client/typescript-angular/model/models.ts
@@ -0,0 +1,6 @@
+export * from './baseError';
+export * from './baseErrorError';
+export * from './newTemplate';
+export * from './template';
+export * from './templateList';
+export * from './user';
