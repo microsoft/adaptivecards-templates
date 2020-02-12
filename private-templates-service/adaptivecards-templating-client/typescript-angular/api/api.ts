b59e2102136e237522a47901700c3cef9765fc91
Grace Zhang <t-grzhan@microsoft.com>
2020-02-11 17:49:48 -0800

    remove unused client sdk

--- private-templates-service/adaptivecards-templating-client/typescript-angular/api/api.ts
+++
@@ -1,5 +0,0 @@
-export * from './template.service';
-import { TemplateService } from './template.service';
-export * from './user.service';
-import { UserService } from './user.service';
-export const APIS = [TemplateService, UserService];
--- private-templates-service/adaptivecards-templating-client/typescript-angular/model/models.ts
+++
@@ -1,6 +0,0 @@
-export * from './baseError';
-export * from './baseErrorError';
-export * from './newTemplate';
-export * from './template';
-export * from './templateList';
-export * from './user';
