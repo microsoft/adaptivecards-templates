5c1115525a71fd19763d801beb7f55660598ed91
Danielle Mott <t-damott@microsoft.com>
2020-02-13 09:36:08 -0800

    Cleanup code

--- private-templates-service/client/src/components/Common/AdaptiveCard/AdaptiveCard.tsx
+++ private-templates-service/client/src/components/Common/AdaptiveCard/AdaptiveCard.tsx
@@ -1,45 +1,8 @@
 import React from 'react';
-
 import * as AdaptiveCards from "adaptivecards";
-
 import { Card } from './styled';
 import markdownit from "markdown-it";
 
-// interface Props {
-//   template: string
-// }
-
-function getCard(): any {
-
-  // Hard coded, will remove and connect to backend in future PR
-  let card = {
-    type: "AdaptiveCard",
-    version: "1.0",
-    body: [
-      {
-        type: "Image",
-        url: "http://adaptivecards.io/content/adaptive-card-50.png"
-      },
-      {
-        type: "TextBlock",
-        text: "Hello **Adaptive Cards!**"
-      }
-    ],
-    actions: [
-      {
-        type: "Action.OpenUrl",
-        title: "Learn more",
-        url: "http://adaptivecards.io"
-      },
-      {
-        type: "Action.OpenUrl",
-        title: "GitHub",
-        url: "http://github.com/Microsoft/AdaptiveCards"
-      }
-    ]
-  };
-  return card;
-}
 function renderingSetup(): AdaptiveCards.AdaptiveCard {
   AdaptiveCards.AdaptiveCard.onProcessMarkdown = function (text: string, result: { didProcess: boolean, outputHtml?: string }) {
     result.outputHtml = new markdownit().render(text);
--- private-templates-service/client/src/components/Gallery/Gallery.tsx
+++ private-templates-service/client/src/components/Gallery/Gallery.tsx
@@ -4,7 +4,6 @@ import { RootState } from '../../store/rootReducer';
 import AdaptiveCardPanel from "../AdaptiveCardPanel";
 import { Container } from "./styled";
 import { getTemplates } from "../../store/templates/actions";
-import { Template } from "adaptive-templating-service-typescript-node";
 
 const mapStateToProps = (state: RootState) => {
   return { state: state };
@@ -26,25 +25,14 @@ class Gallery extends React.Component<Props> {
   componentDidMount() {
     this.props.getTemplates();
   }
-  componentDidUpdate(prevProps: Props) {
-    if (!this.props.state.templates.fetching && this.props.state && this.props.state.templates && this.props.state.templates.templates) {
-      // this.props.state.templates.templates.templates.map((val: any) => console.log((val.instances[0])));
-      // this.props.state.templates.templates.templates.map((val: any) => console.log((val)));
-
-    }
-
-  }
-
   render() {
     let cards = [];
     if (!this.props.state.templates.fetching && this.props.state && this.props.state.templates && this.props.state.templates.templates && this.props.state.templates.templates.templates) {
       cards = this.props.state.templates.templates.templates.map((val: any) => <AdaptiveCardPanel toggleModal={this.props.toggleModal} template={val} />);
     }
-
     return (
       <Container>
         {cards}
-        {/* {cards.slice(0, 3)} */}
       </Container>
     );
   }
