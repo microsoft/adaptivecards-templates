77b105b57183134869ff703afce82cf3f0fde336
Kody Yang <kodyyyang@gmail.com>
2020-02-19 19:40:05 -0800

    resolve conflict

--- private-templates-service/client/src/store/auth/actions.ts
+++ private-templates-service/client/src/store/auth/actions.ts
@@ -1,4 +1,13 @@
-import { LOGIN, LOGOUT, UserType, AuthAction } from './types';
+import {
+  LOGIN,
+  LOGOUT,
+  LOGIN_POPUP_BEGIN,
+  LOGIN_POPUP_FAIL,
+  LOGIN_POPUP_SUCCESS,
+  UserType,
+  AuthAction,
+  LoginPopupAction
+} from './types';
 
 export function login(user: UserType): AuthAction {
   return {
@@ -12,5 +21,32 @@ export function logout(): AuthAction {
   return {
     type: LOGOUT,
     text: 'User logout'
+  }
+}
+
+export function loginPopupBegin(): LoginPopupAction {
+  return {
+    type: LOGIN_POPUP_BEGIN
+  }
+}
+
+export function loginPopupFail(): LoginPopupAction {
+  return {
+    type: LOGIN_POPUP_FAIL
+  }
+}
+
+export function loginPopupSucess(): LoginPopupAction {
+  return {
+    type: LOGIN_POPUP_SUCCESS
+  }
+}
+
+export function loginPopup(): (dispatch: any) => void {
+
+  return function (dispatch: any) {
+    dispatch(loginPopupBegin());
+
+
   }
 }
