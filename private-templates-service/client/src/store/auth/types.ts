421754e23502599566a5dcf300d0d874f5621546
Kody Yang <kodyyyang@gmail.com>
2020-02-19 19:40:05 -0800

    resolve conflict

--- private-templates-service/client/src/store/auth/types.ts
+++ private-templates-service/client/src/store/auth/types.ts
@@ -14,9 +14,17 @@ export interface UserType {
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 
+export const LOGIN_POPUP_BEGIN = 'LOGIN_POPUP_BEGIN';
+export const LOGIN_POPUP_FAIL = 'LOGIN_POPUP_FAIL';
+export const LOGIN_POPUP_SUCCESS = 'LOGIN_POPUP_SUCCESS';
+
 // Actions
 export interface AuthAction {
   type: typeof LOGIN | typeof LOGOUT;
   text: string;
   user?: UserType;
+}
+
+export interface LoginPopupAction {
+  type: typeof LOGIN_POPUP_BEGIN | typeof LOGIN_POPUP_FAIL | typeof LOGIN_POPUP_SUCCESS;
 }
