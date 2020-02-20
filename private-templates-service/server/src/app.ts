fe2802c17b5e9aedc51432d136602014e447e082
Danielle Mott <t-damott@microsoft.com>
2020-02-20 10:27:27 -0800

    Cleanup

--- private-templates-service/server/src/app.ts
+++ private-templates-service/server/src/app.ts
@@ -20,8 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoConnection: MongoConnectionParams = {
-  // connectionString: "#{DBConnection}#"
-  connectionString: "needleZZZZ"
+  connectionString: "__DBConnection__"
 }
 
 const mongoClient: ClientOptions = {
