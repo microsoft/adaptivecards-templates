e80c599a2ff9fd3502c6a87deece3f890863c8d2
Kody Yang <kodyyyang@gmail.com>
2020-02-20 11:31:12 -0800

    Refactor user, org, image details

--- private-templates-service/client/src/Services/GraphService.ts
+++ private-templates-service/client/src/Services/GraphService.ts
@@ -3,7 +3,7 @@ import { Client, AuthProviderCallback } from '@microsoft/microsoft-graph-client'
 import { AuthResponse } from 'msal';
 // var graph = require("@microsoft/microsoft-graph-client");
 
-function getAuthenticatedClient(accessToken: AuthResponse): Client {
+export function getAuthenticatedClient(accessToken: AuthResponse): Client {
   // Initialize Graph client
   const client = Client.init({
     // Use the provided access token to authenticate
@@ -14,19 +14,4 @@ function getAuthenticatedClient(accessToken: AuthResponse): Client {
   });
 
   return client;
-}
-
-// TODO: Refactor into a class, store this in state to eliminate unneccesary retrievals
-export async function getUserDetails(accessToken: AuthResponse) {
-  const client = getAuthenticatedClient(accessToken);
-  const user = await client.api("/me").get();
-  const image = await client.api("/me/photos/240x240/$value").get();
-  user.image = URL.createObjectURL(image);
-  return user;
-}
-
-export async function getOrgDetails(accessToken: AuthResponse) {
-  const client = getAuthenticatedClient(accessToken);
-  const org = await client.api("/organization").get();
-  return org;
 }
--- private-templates-service/client/src/store/auth/types.ts
+++ private-templates-service/client/src/store/auth/types.ts
@@ -1,16 +1,34 @@
+import { AuthResponse } from "msal";
+
 export interface AuthState {
   isAuthenticated: boolean;
   user?: UserType;
+  isFetching: boolean;
+  accessToken?: AuthResponse;
 }
 
 export interface UserType {
   displayName: string;
   email: string;
   organization?: string;
-  avatarURL?: string;
+  imageURL?: string;
 }
 
 // Action Types
+export const ACCESS_TOKEN_SET = 'ACCESS_TOKEN_SET';
+
+export const GET_USER_DETAILS = 'GET_USER_DETAILS';
+export const GET_USER_DETAILS_SUCCESS = 'GET_USER_DETAILS_SUCCESS';
+export const GET_USER_DETAILS_FAILURE = 'GET_USER_DETAILS_FAILURE';
+
+export const GET_ORG_DETAILS = 'GET_ORG_DETAILS';
+export const GET_ORG_DETAILS_SUCCESS = 'GET_ORG_DETAILS_SUCCESS';
+export const GET_ORG_DETAILS_FAILURE = 'GET_ORG_DETAILS_FAILURE';
+
+export const GET_PROFILE_PICTURE = 'GET_PROFILE_PICTURE';
+export const GET_PROFILE_PICTURE_SUCCESS = 'GET_PROFILE_PICTURE_SUCCESS';
+export const GET_PROFILE_PICTURE_FAILURE = 'GET_PROFILE_PICTURE_FAILURE';
+
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 
@@ -20,11 +38,27 @@ export const LOGIN_POPUP_SUCCESS = 'LOGIN_POPUP_SUCCESS';
 
 // Actions
 export interface AuthAction {
-  type: typeof LOGIN | typeof LOGOUT;
+  type: typeof LOGOUT;
   text: string;
   user?: UserType;
 }
 
-export interface LoginPopupAction {
-  type: typeof LOGIN_POPUP_BEGIN | typeof LOGIN_POPUP_FAIL | typeof LOGIN_POPUP_SUCCESS;
+export interface GetUserDetailsAction {
+  type: typeof GET_USER_DETAILS | typeof GET_USER_DETAILS_SUCCESS | typeof GET_USER_DETAILS_FAILURE;
+  user?: UserType;
+}
+
+export interface GetOrgDetailsAction {
+  type: typeof GET_ORG_DETAILS | typeof GET_ORG_DETAILS_SUCCESS | typeof GET_ORG_DETAILS_FAILURE;
+  org?: string;
+}
+
+export interface GetProfilePictureAction {
+  type: typeof GET_PROFILE_PICTURE | typeof GET_PROFILE_PICTURE_SUCCESS | typeof GET_PROFILE_PICTURE_FAILURE;
+  imageURL?: string;
+}
+
+export interface AccessTokenAction {
+  type: typeof ACCESS_TOKEN_SET;
+  accessToken: AuthResponse;
 }
