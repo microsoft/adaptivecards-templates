89635f2ce2bf2108c11216a5e718114f42632ee3
John Travolta <sevkorobot@gmail.com>
2020-02-20 13:29:57 -0800

    Remove dotenv package

--- private-templates-service/adaptivecards-templating-service/src/util/mongoutils/mongoutils.ts
+++ private-templates-service/adaptivecards-templating-service/src/util/mongoutils/mongoutils.ts
@@ -1,15 +1,12 @@
 import mongoose, { Document, Schema, Connection, Model } from "mongoose";
 
 export module MongoUtils {
-  // Base64 Encode
   export function getCollection<T extends Document>(db: Connection, collection: string, schema: Schema): Model<T> {
     return db.model<T>(collection, schema);
   }
-
   export function generateUniqueID(): mongoose.Types.ObjectId {
     return new mongoose.Types.ObjectId();
   }
-
   export function removeUndefinedFields<T>(obj: T): T {
     let result: T = { ...obj };
     for (let key in result) {
