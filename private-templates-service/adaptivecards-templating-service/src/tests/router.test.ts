053b354a91de3e822b4ca6787217a213a1ceb204
Grace Zhang <t-grzhan@microsoft.com>
2020-02-21 14:53:21 -0800

    update tests

--- private-templates-service/adaptivecards-templating-service/src/tests/router.test.ts
+++ private-templates-service/adaptivecards-templating-service/src/tests/router.test.ts
@@ -39,11 +39,11 @@ describe("Basic Post Templates", () => {
   let token: string;
   const app = express();
   let id: string;
+  let idsToDelete: string[] = [];
 
   beforeAll(async () => {
     // TODO: request access token for registered AD app
-    // token = "<INSERT_APP_TOKEN_HERE>";
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSJ9.eyJhdWQiOiIyZTdhYzNmYS1lMTk4LTQ4YmQtYmI2Ny0yZTQzYWU3ZWViMzAiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE1ODIyMjY0ODIsIm5iZiI6MTU4MjIyNjQ4MiwiZXhwIjoxNTgyMjMwMzgyLCJhaW8iOiI0Mk5nWURpOXRmT2FVcVVSZTFiNHp2RFplaVVIQUE9PSIsImF6cCI6IjJlN2FjM2ZhLWUxOTgtNDhiZC1iYjY3LTJlNDNhZTdlZWIzMCIsImF6cGFjciI6IjEiLCJvaWQiOiIyOGVkOGFmYy04ODNiLTRmZDAtYmZjOC1kYWU3NGZkOGFiYjciLCJzdWIiOiIyOGVkOGFmYy04ODNiLTRmZDAtYmZjOC1kYWU3NGZkOGFiYjciLCJ0aWQiOiI3MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDciLCJ1dGkiOiJTM1V4NjQ3NzhVNlhjaDM4NFpzRUFBIiwidmVyIjoiMi4wIn0.sCaGHe9m7eAyzFAPOA1ghuB6stZnGGWFwkS-0dI90hcZRXkylpiyEAHJBEym9xonZA-hIisvE5fX8gfpIVe9Jz1WwIpR47FY4Jh54Gmm84wBXobl9N33MiSA7Et6oKBzgRCZwnPmbiJd7zNFeLgD3gm-Q5Kq3bDkbyU4CFpleEGnHbCpD3yaR4q0meUQEx2-8Mu7yeLVbHaNYhMx6IGogiFRJov_07KMCgPOes1kiMRseddGg_2ARZjz51j3cOVQ4otqBHYtljaea1tyjlNeg6boy0jGVfzrvDvf-RnQ6bD2M1emv9uCB3_t0z8LHsLzyJ8BjOt0srFvy15keo6kKw";
+    token = "<INSERT_APP_TOKEN_HERE>";
     let templateClient = TemplateServiceClient.init(options);
     let middleware: Router = templateClient.expressMiddleware();
     let userMiddleware: Router = templateClient.userExpressMiddleware();
@@ -65,13 +65,16 @@ describe("Basic Post Templates", () => {
 
   // Authenticated post request
   it("should try to post with valid template with default parameters and succeed", async () => {
-    const res = await request(app)
+    let res = await request(app)
       .post("/template")
       .set({ Authorization: "Bearer " + token })
       .send({
         template: "{}"
       });
     expect(res.status).toEqual(201);
+    expect(res.body).toHaveProperty("id");
+    id = res.body.id;
+    idsToDelete.push(id);
 
     // User object should also be created
     const userRes = await request(app)
@@ -97,7 +100,7 @@ describe("Basic Post Templates", () => {
 
   // Template should exist in specific GET by id
   it("should try to get specific template by id and succeed", async () => {
-    const res = await request(app)
+    let res = await request(app)
       .get(`/template/${id}`)
       .set({ Authorization: "Bearer " + token });
     
@@ -124,6 +127,7 @@ describe("Basic Post Templates", () => {
     expect(res.status).toEqual(201);    
     expect(res.body).toHaveProperty("id");
     id = res.body.id;
+    idsToDelete.push(id);
 
     res = await request(app)
     .post(`/template/${id}`)
@@ -133,6 +137,7 @@ describe("Basic Post Templates", () => {
       version: "1.2"
     });
     expect(res.status).toEqual(201); 
+    idsToDelete.push(res.body.id);
 
     res = await request(app)
       .get("/template")
@@ -173,19 +178,21 @@ describe("Basic Post Templates", () => {
 
   afterAll(async () => {
     await mongoose.connection.close();
+    for (let id of idsToDelete){
+      await request(app).delete(`/template/${id}`);
+    }
   });
 });
-
 
 describe("Basic Get Templates", () => {
   let token: string;
   const app = express();
   let id: string;
+  let idsToDelete: string[] = [];
 
   beforeAll(async () => {
     // TODO: request access token for registered AD app
-    // token = "<INSERT_APP_TOKEN_HERE>";
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSJ9.eyJhdWQiOiIyZTdhYzNmYS1lMTk4LTQ4YmQtYmI2Ny0yZTQzYWU3ZWViMzAiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE1ODIyMjY0ODIsIm5iZiI6MTU4MjIyNjQ4MiwiZXhwIjoxNTgyMjMwMzgyLCJhaW8iOiI0Mk5nWURpOXRmT2FVcVVSZTFiNHp2RFplaVVIQUE9PSIsImF6cCI6IjJlN2FjM2ZhLWUxOTgtNDhiZC1iYjY3LTJlNDNhZTdlZWIzMCIsImF6cGFjciI6IjEiLCJvaWQiOiIyOGVkOGFmYy04ODNiLTRmZDAtYmZjOC1kYWU3NGZkOGFiYjciLCJzdWIiOiIyOGVkOGFmYy04ODNiLTRmZDAtYmZjOC1kYWU3NGZkOGFiYjciLCJ0aWQiOiI3MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDciLCJ1dGkiOiJTM1V4NjQ3NzhVNlhjaDM4NFpzRUFBIiwidmVyIjoiMi4wIn0.sCaGHe9m7eAyzFAPOA1ghuB6stZnGGWFwkS-0dI90hcZRXkylpiyEAHJBEym9xonZA-hIisvE5fX8gfpIVe9Jz1WwIpR47FY4Jh54Gmm84wBXobl9N33MiSA7Et6oKBzgRCZwnPmbiJd7zNFeLgD3gm-Q5Kq3bDkbyU4CFpleEGnHbCpD3yaR4q0meUQEx2-8Mu7yeLVbHaNYhMx6IGogiFRJov_07KMCgPOes1kiMRseddGg_2ARZjz51j3cOVQ4otqBHYtljaea1tyjlNeg6boy0jGVfzrvDvf-RnQ6bD2M1emv9uCB3_t0z8LHsLzyJ8BjOt0srFvy15keo6kKw";
+    token = "<INSERT_APP_TOKEN_HERE>";
     let templateClient = TemplateServiceClient.init(options);
     let middleware: Router = templateClient.expressMiddleware();
     let userMiddleware: Router = templateClient.userExpressMiddleware();
@@ -220,6 +227,7 @@ describe("Basic Get Templates", () => {
     expect(res.status).toEqual(201);    
     expect(res.body).toHaveProperty("id");
     id = res.body.id;
+    idsToDelete.push(id);
 
     res = await request(app)
     .post(`/template/${id}`)
@@ -229,6 +237,7 @@ describe("Basic Get Templates", () => {
       version: "1.2"
     });
     expect(res.status).toEqual(201); 
+    idsToDelete.push(id);
 
     res = await request(app)
       .get("/template")
@@ -255,86 +264,21 @@ describe("Basic Get Templates", () => {
   });
 
   afterAll(async () => {
-    await mongoose.connection.close();
+    for (let id of idsToDelete){
+      await request(app).delete(`/template/${id}`);
+    }
   });
-});
-
-describe("Get Tags", () => {
-  let token: string;
-  const app = express();
-  let id: string;
-
-  beforeAll(async () => {
-    // TODO: request access token for registered AD app
-    // token = "<INSERT_APP_TOKEN_HERE>";
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSJ9.eyJhdWQiOiIyZTdhYzNmYS1lMTk4LTQ4YmQtYmI2Ny0yZTQzYWU3ZWViMzAiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE1ODIyMjY0ODIsIm5iZiI6MTU4MjIyNjQ4MiwiZXhwIjoxNTgyMjMwMzgyLCJhaW8iOiI0Mk5nWURpOXRmT2FVcVVSZTFiNHp2RFplaVVIQUE9PSIsImF6cCI6IjJlN2FjM2ZhLWUxOTgtNDhiZC1iYjY3LTJlNDNhZTdlZWIzMCIsImF6cGFjciI6IjEiLCJvaWQiOiIyOGVkOGFmYy04ODNiLTRmZDAtYmZjOC1kYWU3NGZkOGFiYjciLCJzdWIiOiIyOGVkOGFmYy04ODNiLTRmZDAtYmZjOC1kYWU3NGZkOGFiYjciLCJ0aWQiOiI3MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDciLCJ1dGkiOiJTM1V4NjQ3NzhVNlhjaDM4NFpzRUFBIiwidmVyIjoiMi4wIn0.sCaGHe9m7eAyzFAPOA1ghuB6stZnGGWFwkS-0dI90hcZRXkylpiyEAHJBEym9xonZA-hIisvE5fX8gfpIVe9Jz1WwIpR47FY4Jh54Gmm84wBXobl9N33MiSA7Et6oKBzgRCZwnPmbiJd7zNFeLgD3gm-Q5Kq3bDkbyU4CFpleEGnHbCpD3yaR4q0meUQEx2-8Mu7yeLVbHaNYhMx6IGogiFRJov_07KMCgPOes1kiMRseddGg_2ARZjz51j3cOVQ4otqBHYtljaea1tyjlNeg6boy0jGVfzrvDvf-RnQ6bD2M1emv9uCB3_t0z8LHsLzyJ8BjOt0srFvy15keo6kKw";
-    let templateClient = TemplateServiceClient.init(options);
-    let middleware: Router = templateClient.expressMiddleware();
-    app.use(bodyParser.json());
-    app.use("/template", middleware);
-  });
-
-  it("should try to post tags with a template and succeed", async () => {
-    let res = await request(app)
-      .post("/template")
-      .set({ Authorization: "Bearer " + token })
-      .send({
-        template: "{}",
-        tags: [
-          "Weather"
-        ],
-        isPublished: false
-      });
-    expect(res.status).toEqual(201);
-    expect(res.body).toHaveProperty("id");
-    id = res.body.id;
-    
-    res = await request(app)
-      .get(`/template/${id}`)
-    expect(res.body).toHaveProperty("templates");
-    expect(res.body.templates).toHaveLength(1);
-    let template = res.body.templates[0];
-    expect(template.tags).toHaveLength(1);
-    expect(template.tags[0]).toEqual("Weather");
-
-    res = await request(app)
-    .post("/template")
-    .set({ Authorization: "Bearer " + token })
-    .send({
-      template: "{}",
-      tags: [
-        "Contosa"
-      ],
-      isPublished: true
-    });
-    expect(res.status).toEqual(201);
-  });
-
-  // Authenticated post request
-  it("should try to get the list of owners and all tags and succeed", async () => {
-    let res = await request(app)
-      .get("/template/tag");
-    expect(res.status).toEqual(200);
-    expect(res.body).toHaveProperty("ownedTags");
-    expect(res.body).toHaveProperty("allTags");
-    expect(res.body.ownedTags).toHaveLength(2);
-    expect(res.body.ownedTags).toContain("Weather");
-    expect(res.body.ownedTags).toContain("Contosa");
-    expect(res.body.allTags).toHaveLength(1);
-    expect(res.body.allTags).toContain("Contosa");
-  });
-  
 });
 
 describe("Preview Templates", () => {
   let token: string;
   const app = express();
   let id: string;
+  let idsToDelete: string[] = [];
 
   beforeAll(async () => {
     // TODO: request access token for registered AD app
-    // token = "<INSERT_APP_TOKEN_HERE>";
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSJ9.eyJhdWQiOiIyZTdhYzNmYS1lMTk4LTQ4YmQtYmI2Ny0yZTQzYWU3ZWViMzAiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE1ODIyMjY0ODIsIm5iZiI6MTU4MjIyNjQ4MiwiZXhwIjoxNTgyMjMwMzgyLCJhaW8iOiI0Mk5nWURpOXRmT2FVcVVSZTFiNHp2RFplaVVIQUE9PSIsImF6cCI6IjJlN2FjM2ZhLWUxOTgtNDhiZC1iYjY3LTJlNDNhZTdlZWIzMCIsImF6cGFjciI6IjEiLCJvaWQiOiIyOGVkOGFmYy04ODNiLTRmZDAtYmZjOC1kYWU3NGZkOGFiYjciLCJzdWIiOiIyOGVkOGFmYy04ODNiLTRmZDAtYmZjOC1kYWU3NGZkOGFiYjciLCJ0aWQiOiI3MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDciLCJ1dGkiOiJTM1V4NjQ3NzhVNlhjaDM4NFpzRUFBIiwidmVyIjoiMi4wIn0.sCaGHe9m7eAyzFAPOA1ghuB6stZnGGWFwkS-0dI90hcZRXkylpiyEAHJBEym9xonZA-hIisvE5fX8gfpIVe9Jz1WwIpR47FY4Jh54Gmm84wBXobl9N33MiSA7Et6oKBzgRCZwnPmbiJd7zNFeLgD3gm-Q5Kq3bDkbyU4CFpleEGnHbCpD3yaR4q0meUQEx2-8Mu7yeLVbHaNYhMx6IGogiFRJov_07KMCgPOes1kiMRseddGg_2ARZjz51j3cOVQ4otqBHYtljaea1tyjlNeg6boy0jGVfzrvDvf-RnQ6bD2M1emv9uCB3_t0z8LHsLzyJ8BjOt0srFvy15keo6kKw";
+    token = "<INSERT_APP_TOKEN_HERE>";
     let templateClient = TemplateServiceClient.init(options);
     let middleware: Router = templateClient.expressMiddleware();
     app.use(bodyParser.json());
@@ -353,6 +297,7 @@ describe("Preview Templates", () => {
     expect(res.status).toEqual(201);
     expect(res.body).toHaveProperty("id");
     id = res.body.id;
+    idsToDelete.push(id);
     
     res = await request(app)
       .get(`/template/${id}/preview?version=1.2`)
@@ -394,6 +339,7 @@ describe("Preview Templates", () => {
     expect(res.status).toEqual(201);
     expect(res.body).toHaveProperty("id");
     id = res.body.id;
+    idsToDelete.push(id);
 
     res = await request(app)
       .get(`/template/${id}`);
@@ -409,17 +355,23 @@ describe("Preview Templates", () => {
       .get(`/template/${id}/preview?version=1.0`);
     expect(res.status).toEqual(404);
   });
+
+  afterAll(async () => {
+    for (let id of idsToDelete){
+      await request(app).delete(`/template/${id}`);
+    }
+  });
 });
 
 describe("Delete Templates", () => {
   let token: string;
   const app = express();
   let id: string;
+  let idsToDelete: string[] = [];
 
   beforeAll(async () => {
     // TODO: request access token for registered AD app
-    // token = "<INSERT_APP_TOKEN_HERE>";
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSJ9.eyJhdWQiOiIyZTdhYzNmYS1lMTk4LTQ4YmQtYmI2Ny0yZTQzYWU3ZWViMzAiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3L3YyLjAiLCJpYXQiOjE1ODIyMjY0ODIsIm5iZiI6MTU4MjIyNjQ4MiwiZXhwIjoxNTgyMjMwMzgyLCJhaW8iOiI0Mk5nWURpOXRmT2FVcVVSZTFiNHp2RFplaVVIQUE9PSIsImF6cCI6IjJlN2FjM2ZhLWUxOTgtNDhiZC1iYjY3LTJlNDNhZTdlZWIzMCIsImF6cGFjciI6IjEiLCJvaWQiOiIyOGVkOGFmYy04ODNiLTRmZDAtYmZjOC1kYWU3NGZkOGFiYjciLCJzdWIiOiIyOGVkOGFmYy04ODNiLTRmZDAtYmZjOC1kYWU3NGZkOGFiYjciLCJ0aWQiOiI3MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDciLCJ1dGkiOiJTM1V4NjQ3NzhVNlhjaDM4NFpzRUFBIiwidmVyIjoiMi4wIn0.sCaGHe9m7eAyzFAPOA1ghuB6stZnGGWFwkS-0dI90hcZRXkylpiyEAHJBEym9xonZA-hIisvE5fX8gfpIVe9Jz1WwIpR47FY4Jh54Gmm84wBXobl9N33MiSA7Et6oKBzgRCZwnPmbiJd7zNFeLgD3gm-Q5Kq3bDkbyU4CFpleEGnHbCpD3yaR4q0meUQEx2-8Mu7yeLVbHaNYhMx6IGogiFRJov_07KMCgPOes1kiMRseddGg_2ARZjz51j3cOVQ4otqBHYtljaea1tyjlNeg6boy0jGVfzrvDvf-RnQ6bD2M1emv9uCB3_t0z8LHsLzyJ8BjOt0srFvy15keo6kKw";
+    token = "<INSERT_APP_TOKEN_HERE>";
     let templateClient = TemplateServiceClient.init(options);
     let middleware: Router = templateClient.expressMiddleware();
     app.use(bodyParser.json());
@@ -445,4 +397,182 @@ describe("Delete Templates", () => {
       .get(`/template/${id}`);
     expect(res.status).toEqual(404);
   });
-})
+
+  it("should try to delete a specific version of the posted template and succeed", async () => {
+    let res = await request(app)
+      .post("/template")
+      .set({ Authorization: "Bearer " + token })
+      .send({
+        template: "{}"
+      });
+    expect(res.status).toEqual(201);
+    expect(res.body).toHaveProperty("id");
+    id = res.body.id;
+    idsToDelete.push(id);
+
+    res = await request(app)
+    .post(`/template/${id}`)
+    .set({ Authorization: "Bearer " + token })
+    .send({
+      template: "{}",
+      version: "1.4"
+    });
+    expect(res.status).toEqual(201);
+
+    res = await request(app)
+      .delete(`/template/${id}?version=1.4`);
+    expect(res.status).toEqual(204);
+
+    res = await request(app)
+      .get(`/template/${id}?version=1.4`);
+    expect(res.status).toEqual(404);
+
+    res = await request(app)
+    .get(`/template/${id}`);
+    expect(res.status).toEqual(200);
+    expect(res.body.templates[0].instances[0].version).toEqual("1.0");
+  });
+
+  afterAll(async () => {
+    for (let id of idsToDelete){
+      await request(app).delete(`/template/${id}`);
+    }
+  });
+});
+
+describe("Filtering Templates", () => {
+  let token: string;
+  const app = express();
+  let id: string;
+  let idsToDelete: string[] = [];
+
+  beforeAll(async () => {
+    // TODO: request access token for registered AD app
+    token = "<INSERT_APP_TOKEN_HERE>";
+    let templateClient = TemplateServiceClient.init(options);
+    let middleware: Router = templateClient.expressMiddleware();
+    app.use(bodyParser.json());
+    app.use("/template", middleware);
+  });
+
+  it("should try to filter template by tag and succeed", async () => {
+    let res = await request(app)
+    .post("/template")
+    .set({ Authorization: "Bearer " + token })
+    .send({
+      template: "{}",
+      tags: [
+        "Weather"
+      ]
+    });
+    expect(res.status).toEqual(201);
+    expect(res.body).toHaveProperty("id");
+    id = res.body.id;
+    idsToDelete.push(id);
+
+    res = await request(app)
+    .post("/template")
+    .set({ Authorization: "Bearer " + token })
+    .send({
+      template: "{}",
+      tags: [
+        "Sunny"
+      ]
+    });
+    expect(res.status).toEqual(201);
+    idsToDelete.push(res.body.id);
+
+    res = await request(app)
+      .get("/template")
+      .set({ Authorization: "Bearer " + token })
+      .send({
+        tags: [
+          "Weather"
+        ]
+      });
+    expect(res.status).toEqual(200);
+    expect(res.body.templates).toHaveLength(1);
+  });
+
+  afterAll(async () => {
+    for (let id of idsToDelete){
+      await request(app).delete(`/template/${id}`);
+    }
+  });
+});
+
+describe("Get Tags", () => {
+  let token: string;
+  const app = express();
+  let id: string;
+  let idsToDelete: string[] = [];
+
+  beforeAll(async () => {
+    // TODO: request access token for registered AD app
+    token = "<INSERT_APP_TOKEN_HERE>";
+    let templateClient = TemplateServiceClient.init(options);
+    let middleware: Router = templateClient.expressMiddleware();
+    app.use(bodyParser.json());
+    app.use("/template", middleware);
+  });
+
+  it("should try to post tags with a template and succeed", async () => {
+    let res = await request(app)
+      .post("/template")
+      .set({ Authorization: "Bearer " + token })
+      .send({
+        template: "{}",
+        tags: [
+          "Weather"
+        ],
+        isPublished: false
+      });
+    expect(res.status).toEqual(201);
+    expect(res.body).toHaveProperty("id");
+    id = res.body.id;
+    idsToDelete.push(id);
+    
+    res = await request(app)
+      .get(`/template/${id}`)
+    expect(res.body).toHaveProperty("templates");
+    expect(res.body.templates).toHaveLength(1);
+    let template = res.body.templates[0];
+    expect(template.tags).toHaveLength(1);
+    expect(template.tags[0]).toEqual("Weather");
+
+    res = await request(app)
+    .post("/template")
+    .set({ Authorization: "Bearer " + token })
+    .send({
+      template: "{}",
+      tags: [
+        "Contosa"
+      ],
+      isPublished: true
+    });
+    expect(res.status).toEqual(201);    
+    expect(res.body).toHaveProperty("id");
+    await request(app).delete(`/template/${res.body.id}`);
+    idsToDelete.push(res.body.id);
+  });
+
+  // Authenticated post request
+  it("should try to get the list of owners and all tags and succeed", async () => {
+    let res = await request(app)
+      .get("/template/tag");
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty("ownedTags");
+    expect(res.body).toHaveProperty("allTags");
+    expect(res.body.ownedTags).toHaveLength(2);
+    expect(res.body.ownedTags).toContain("Weather");
+    expect(res.body.ownedTags).toContain("Contosa");
+    expect(res.body.allTags).toHaveLength(1);
+    expect(res.body.allTags).toContain("Contosa");
+  });
+
+  afterAll(async () => {
+    for (let id of idsToDelete){
+      await request(app).delete(`/template/${id}`);
+    }
+  });
+});
