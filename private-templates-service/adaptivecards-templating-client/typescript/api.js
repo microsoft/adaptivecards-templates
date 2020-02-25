b1fae91270a7306e693d73fe9ac3401d7c874911
Danielle Mott <t-damott@microsoft.com>
2020-02-25 09:15:51 -0800

    Change defaultBasePath back to dev website

--- private-templates-service/adaptivecards-templating-client/typescript/api.js
+++ private-templates-service/adaptivecards-templating-client/typescript/api.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var localVarRequest = require("request");
 var Promise = require("bluebird");
-var defaultBasePath = 'http://localhost:3000';
+var defaultBasePath = 'http://adaptivecms.azurewebsites.net';
 var primitives = [
     "string",
     "boolean",
