ba767b31c47561ddfefdcbbf01f9d31a5060ce93
Kody Yang <kodyyyang@gmail.com>
2020-03-04 13:02:24 -0800

    Add fallback GET request for client

--- private-templates-service/server/src/app.ts
+++ private-templates-service/server/src/app.ts
@@ -8,11 +8,14 @@ import { TemplateServiceClient } from "../../adaptivecards-templating-service/sr
 import { ClientOptions } from "../../adaptivecards-templating-service/src/IClientOptions";
 import { AzureADProvider } from "../../adaptivecards-templating-service/src/authproviders/AzureADProvider";
 import { MongoDBProvider } from "../../adaptivecards-templating-service/src/storageproviders/MongoDBProvider";
+
+const RELATIVE_PATH_CLIENT = '../../../../client/build';
+
 // import mongo, create new mongo, pass options
 const app = express();
 
 // Express configuration
-app.use(express.static(path.join(__dirname, "../../../../client/build")));
+app.use(express.static(path.join(__dirname, RELATIVE_PATH_CLIENT)));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
@@ -31,6 +34,10 @@ mongoDB.connect()
         const client: TemplateServiceClient = TemplateServiceClient.init(mongoClient);
         app.use("/template", client.expressMiddleware());
         app.use("/user", client.userExpressMiddleware());
+
+        app.get('*', (req, res) => {
+          res.sendFile(path.join(__dirname, RELATIVE_PATH_CLIENT + '/index.html'));
+        })
       } else {
         console.log(res.errorMessage);
       }
