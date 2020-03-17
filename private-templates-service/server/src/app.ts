adedfeb414f04517ba19abdb867b4b7220a2dd25
Grace Zhang <31353754+gracez72@users.noreply.github.com>
2020-03-16 17:26:28 -0700

    Configure cookies for HttpOnly and secure cookies (#146)
    * secure cookies & httponly
    
    * fix warnings
    
    * add hsts header
    
    * remove subdomain check
    
    Co-authored-by: Grace Zhang <t-grzhan@microsoft.com>

--- private-templates-service/server/src/app.ts
+++ private-templates-service/server/src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import path from "path";
 import passport from "./config/passport";
 import bodyParser from "body-parser";
+import session from "express-session";
+import helmet from "helmet";
 
 // import controllers
 import { TemplateServiceClient } from "../../adaptivecards-templating-service/src/TemplateServiceClient";
@@ -20,6 +22,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet.hsts({
+  maxAge: 15552000
+}));
+app.use(session({
+  secret: '#{CLIENT_ID_TOKEN}#',
+  cookie: {
+    httpOnly: true,
+    secure: true
+  },
+  resave: false,
+  saveUninitialized: true
+}));
 
 let mongoDB = new MongoDBProvider({ connectionString: "#{DB_CONNECTION_TOKEN}#" });
 mongoDB.connect()
