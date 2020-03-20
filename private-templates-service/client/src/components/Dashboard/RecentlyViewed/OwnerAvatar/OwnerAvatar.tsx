95f3bd09bfe65abfb62d2dfe41d5ebeea665b24e
Danielle Mott <t-damott@microsoft.com>
2020-03-20 13:13:56 -0700

    Clean up comment in OwnerAvatar

--- private-templates-service/client/src/components/Dashboard/RecentlyViewed/OwnerAvatar/OwnerAvatar.tsx
+++ private-templates-service/client/src/components/Dashboard/RecentlyViewed/OwnerAvatar/OwnerAvatar.tsx
@@ -17,24 +17,22 @@ const mapStateToProps = (state: RootState) => {
 };
 
 class OwnerAvatar extends React.Component<Props> {
-  // If a user avatar is available, return an img tag with the pic
-
   render() {
-      if (this.props.owner && this.props.owner.imageURL){
-        let facepileProps: IFacepileProps = {
-          personaSize: PersonaSize.size24,
-          personas: new Array({imageUrl: this.props.owner.imageURL, }),
-        };
-        return (<Facepile {...facepileProps} />)
-      }
-      else if (this.props.owner && this.props.owner.displayName && this.props.owner.displayName[0]) {
-        let facepileProps: IFacepileProps = {
-          personaSize: PersonaSize.size24,
-          personas: new Array({imageInitials: this.props.owner.displayName[0], }),
-        };
-        return (<Facepile {...facepileProps} />)
-      }
-      return (<div>Error loading image</div>)
+    if (this.props.owner && this.props.owner.imageURL) {
+      let facepileProps: IFacepileProps = {
+        personaSize: PersonaSize.size24,
+        personas: new Array({ imageUrl: this.props.owner.imageURL, }),
+      };
+      return (<Facepile {...facepileProps} />)
+    }
+    else if (this.props.owner && this.props.owner.displayName && this.props.owner.displayName[0]) {
+      let facepileProps: IFacepileProps = {
+        personaSize: PersonaSize.size24,
+        personas: new Array({ imageInitials: this.props.owner.displayName[0], }),
+      };
+      return (<Facepile {...facepileProps} />)
+    }
+    return (<div>Error loading image</div>)
   }
 }
 
