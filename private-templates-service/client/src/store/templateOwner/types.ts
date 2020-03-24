ab254a933f2317effe76bb6c9624f074c539a98c
Danielle Mott <t-damott@microsoft.com>
2020-03-24 10:22:23 -0700

    Add owner name and picture (#167)
    * Call Microsoft graph to get user name and pic
    
    * Changes to fetching owner
    
    * Problem with Microsoft graph call
    
    * Get owner name and pic
    
    * Fix infinite calls to microsoft graph
    
    * Refactor
    
    * Fix merge conflicts
    
    * add newlien
    
    * Clean up comment in OwnerAvatar
    
    * Remove unnecessary import
    
    * Address Seva's comments
    
    * Seva's comments
    
    * Remove newline
    
    * formatting
    
    * Create list in Owner type
    
    * Clear owners list on Get call
    
    * Clear Owners dictionary when dashboard is visited
    
    * remove console log

---
+++ private-templates-service/client/src/store/templateOwner/types.ts
@@ -0,0 +1,35 @@
+export interface OwnerState {
+  owners?: OwnerType;
+  isFetching: boolean;
+}
+
+export interface OwnerType {
+  displayNames?: { [index: number]: string; };
+  imageURLs?: { [index: number]: string; };
+}
+
+export const GET_OWNER_NAME = 'GET_OWNER_NAME';
+export const GET_OWNER_NAME_SUCCESS = 'GET_OWNER_NAME_SUCCESS';
+export const GET_OWNER_NAME_FAILURE = 'GET_OWNER_NAME_FAILURE';
+
+export const GET_OWNER_PROFILE_PICTURE = 'GET_OWNER_PROFILE_PICTURE';
+export const GET_OWNER_PROFILE_PICTURE_SUCCESS = 'GET_OWNER_PROFILE_PICTURE_SUCCESS';
+export const GET_OWNER_PROFILE_PICTURE_FAILURE = 'GET_OWNER_PROFILE_PICTURE_FAILURE';
+
+export const CLEAR_OWNERS = 'CLEAR_OWNERS';
+
+export interface GetOwnerNameAction {
+  type: typeof GET_OWNER_NAME | typeof GET_OWNER_NAME_SUCCESS | typeof GET_OWNER_NAME_FAILURE;
+  index?: number;
+  ownerName?: string;
+}
+
+export interface GetOwnerProfilePictureAction {
+  type: typeof GET_OWNER_PROFILE_PICTURE | typeof GET_OWNER_PROFILE_PICTURE_SUCCESS | typeof GET_OWNER_PROFILE_PICTURE_FAILURE;
+  index?: number;
+  ownerImageURL?: string;
+}
+
+export interface ClearOwnersAction {
+  type: typeof CLEAR_OWNERS;
+}
