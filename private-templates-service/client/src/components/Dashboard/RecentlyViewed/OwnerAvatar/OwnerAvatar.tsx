6969b3a32001d55b5aa66a744c1e66920a84ccfe
Danielle Mott <t-damott@microsoft.com>
2020-03-26 14:27:56 -0700

    change aria label

--- private-templates-service/client/src/components/Dashboard/RecentlyViewed/OwnerAvatar/OwnerAvatar.tsx
+++ private-templates-service/client/src/components/Dashboard/RecentlyViewed/OwnerAvatar/OwnerAvatar.tsx
@@ -25,14 +25,14 @@ class OwnerAvatar extends React.Component<Props> {
         personaSize: PersonaSize.size24,
         personas: new Array({ imageUrl: this.props.owner.imageURLs[this.props.index], }),
       };
-      return (<Facepile {...facepileProps} ariaLabel={"Facepile displaying profile pictures"} />)
+      return (<Facepile {...facepileProps} ariaLabel="Facepile displaying profile pictures" />)
     }
     else if (this.props.owner && this.props.owner.displayNames && this.props.owner.displayNames[this.props.index] && this.props.owner.displayNames[this.props.index][0]) {
       let facepileProps: IFacepileProps = {
         personaSize: PersonaSize.size24,
         personas: new Array({ imageInitials: this.props.owner.displayNames[this.props.index][0], }),
       };
-      return (<Facepile {...facepileProps} ariaLabel={"Facepile displaying profile pictures"} />)
+      return (<Facepile {...facepileProps} ariaLabel="Facepile displaying profile pictures" />)
     }
     return (<div>Error loading image</div>)
   }
