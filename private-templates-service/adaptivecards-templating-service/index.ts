665eefb697cad02ec8fc9211db33ae1cfcfa0918
Grace Zhang <t-grzhan@microsoft.com>
2020-04-03 11:33:08 -0700

    readd index

---
+++ private-templates-service/adaptivecards-templating-service/index.ts
@@ -0,0 +1,11 @@
+import { TemplateServiceClient } from './src/TemplateServiceClient'
+import { ClientOptions } from './src/IClientOptions';
+import { AzureADProvider } from './src/authproviders/AzureADProvider';
+import { InMemoryDBProvider } from './src/storageproviders/InMemoryDBProvider';
+import { MongoDBProvider } from './src/storageproviders/MongoDBProvider';
+
+export { TemplateServiceClient };
+export { ClientOptions };
+export { AzureADProvider };
+export { InMemoryDBProvider };
+export { MongoDBProvider };
