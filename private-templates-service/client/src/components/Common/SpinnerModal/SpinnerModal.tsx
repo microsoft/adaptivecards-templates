739888a0ed095933fc23ba06617f0d2a7fb29591
nnmkhang <nnmkhang166@gmail.com>
2020-04-06 14:04:19 -0700

    removing background on spinner

--- private-templates-service/client/src/components/Common/SpinnerModal/SpinnerModal.tsx
+++ private-templates-service/client/src/components/Common/SpinnerModal/SpinnerModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CenteredSpinner, Dot } from './styled';
+import { CenteredSpinner } from './styled';
 import { BackDrop } from '../ShareModal/styled';
 import { SpinnerSize } from 'office-ui-fabric-react';
 
@@ -7,9 +7,7 @@ class SpinnerModal extends React.Component{
   render() {
     return (
       <BackDrop>
-        <Dot>
-          <CenteredSpinner size={SpinnerSize.large}/>
-        </Dot>
+        <CenteredSpinner size={SpinnerSize.large}/>
       </BackDrop>
     );
   }
--- private-templates-service/client/src/components/Common/SpinnerModal/styled.tsx
+++ private-templates-service/client/src/components/Common/SpinnerModal/styled.tsx
@@ -5,12 +5,3 @@ import { COLORS } from '../../../globalStyles';
 export const CenteredSpinner = styled(Spinner)`
   margin: auto;
 `;
-
-export const Dot = styled.div`
-  height: 30px;
-  width: 30px;
-  background-color: ${COLORS.WHITE};
-  border-radius: 50%;
-  display: flex;
-  justify-content: centre;
-`;
