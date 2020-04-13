0d2f3c3af7a8f725e4e14d696048a94659d710d5
John Travolta <sevkorobot@gmail.com>
2020-04-13 11:55:21 -0700

    Fix styling

--- private-templates-service/client/src/components/AllCards/AllCards.tsx
+++ private-templates-service/client/src/components/AllCards/AllCards.tsx
@@ -1,36 +1,44 @@
 // React
-import React, { Component } from 'react'
-import { withRouter, RouteComponentProps } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { withRouter, RouteComponentProps } from "react-router-dom";
+import { connect } from "react-redux";
 // Components
-import TemplatesPage from '../Common/TemplatesPage';
+import TemplatesPage from "../Common/TemplatesPage";
 // Redux Store
-import { getAllTags } from '../../store/tags/actions'
-import { getAllTemplates } from '../../store/templates/actions'
+import { getAllTags } from "../../store/tags/actions";
+import { getAllTemplates } from "../../store/templates/actions";
 // Strings
-import {ALL_CARDS, ALL_CARDS_TITLE} from '../../assets/strings'
+import { ALL_CARDS, ALL_CARDS_TITLE } from "../../assets/strings";
 // Utils
-import requireAuthentication from '../../utils/requireAuthentication';
+import requireAuthentication from "../../utils/requireAuthentication";
 const mapDispatchToProps = (dispatch: any) => {
-    return {
-      getTemplates: (tags?: string[]) => {
-        dispatch(getAllTemplates(tags));
-      },
-      getTags: () => {
-        dispatch(getAllTags());
-      }
-    };
+  return {
+    getTemplates: (tags?: string[]) => {
+      dispatch(getAllTemplates(tags));
+    },
+    getTags: () => {
+      dispatch(getAllTags());
+    },
+  };
 };
 
 interface Props extends RouteComponentProps {
-    getTemplates: (tags?: string[]) => void;
-    getTags: () => void;
+  getTemplates: (tags?: string[]) => void;
+  getTags: () => void;
 }
 export class AllCards extends Component<Props> {
-    render() {
-        return (
-            <TemplatesPage getTags={this.props.getTags} getTemplates={this.props.getTemplates} pageTitle={ALL_CARDS_TITLE} pageID={ALL_CARDS}/>
-        )
-    }
+  render() {
+    return (
+      <TemplatesPage
+        getTags={this.props.getTags}
+        getTemplates={this.props.getTemplates}
+        pageTitle={ALL_CARDS_TITLE}
+        pageID={ALL_CARDS}
+      />
+    );
+  }
 }
-export default connect(null, mapDispatchToProps)(requireAuthentication(withRouter(AllCards)));
+export default connect(
+  null,
+  mapDispatchToProps
+)(requireAuthentication(withRouter(AllCards)));
