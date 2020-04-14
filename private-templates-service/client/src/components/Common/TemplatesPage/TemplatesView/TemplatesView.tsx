dbf8463b23bc3811b90baffe9ca0ab9f9c9c2c72
guptaharsh25 <me@harshgupta.ca>
2020-04-14 14:27:23 -0700

    added tooltips and removed console warnings

--- private-templates-service/client/src/components/Common/TemplatesPage/TemplatesView/TemplatesView.tsx
+++ private-templates-service/client/src/components/Common/TemplatesPage/TemplatesView/TemplatesView.tsx
@@ -28,16 +28,13 @@ interface Props {
 }
 
 export class TemplatesView extends Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
- 
+
   componentDidMount() {
     this.props.getTemplates(this.props.selectedTags);
   }
 
   componentDidUpdate(prevProps: Props) {
-    if(prevProps.selectedTags.length != this.props.selectedTags.length) {
+    if (prevProps.selectedTags.length !== this.props.selectedTags.length) {
       this.props.getTemplates(this.props.selectedTags);
     }
   }
@@ -57,11 +54,11 @@ export class TemplatesView extends Component<Props> {
           toggleState.viewType === ViewType.List ? (
             <TemplateList templates={templates} displayComponents={{ author: true, dateModified: true, templateName: true, status: true, version: false }} onClick={onClick} />
           ) : (
-            <Gallery onClick={onClick} templates={templates} />
-          )
+              <Gallery onClick={onClick} templates={templates} />
+            )
         ) : (
-          <PlaceholderText>{ALL_CARDS_PLACEHOLDER}</PlaceholderText>
-        )}
+              <PlaceholderText>{ALL_CARDS_PLACEHOLDER}</PlaceholderText>
+            )}
       </React.Fragment>
     );
   }
