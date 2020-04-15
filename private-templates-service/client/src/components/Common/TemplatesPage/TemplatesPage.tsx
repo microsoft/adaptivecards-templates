9d07640ef1a49ece3b47f18b32a10e595c7b7ee7
Harsh Gupta <reyharsh25@gmail.com>
2020-04-15 13:36:22 -0400

    Tooltips on All Cards page (#239)
    * added tooltips and removed console warnings
    
    * remove setState declarations

--- private-templates-service/client/src/components/Common/TemplatesPage/TemplatesPage.tsx
+++ private-templates-service/client/src/components/Common/TemplatesPage/TemplatesPage.tsx
@@ -23,6 +23,7 @@ import SearchPage from "../../Dashboard/SearchPage";
 import { LIST_VIEW, GRID_VIEW } from "../../../assets/strings";
 import TagList from "./TagList";
 import { COLORS } from "../../../globalStyles";
+import { TooltipHost } from "office-ui-fabric-react";
 
 const mapStateToProps = (state: RootState) => {
   return {
@@ -140,6 +141,9 @@ class TemplatesPage extends Component<Props, State> {
     if (!tagsState.isFetching && tagsState.allTags && tagsState.allTags.allTags) {
       allTags = tagsState.allTags?.allTags;
     }
+    let listTooltip = "listTooltip";
+    let gridTooltip = "gridTooltip";
+
 
     return (
       <OuterCardsContainer>
@@ -147,14 +151,18 @@ class TemplatesPage extends Component<Props, State> {
           <UpperBar>
             <Title>{this.props.pageTitle}</Title>
             <ViewHelperBar>
-              <ToggleButton iconProps={{ iconName: "BulletedList" }} onClick={this.props.toggleView} viewType={ViewType.List} title={LIST_VIEW} />
-              <ToggleButton iconProps={{ iconName: "GridViewMedium" }} onClick={this.props.toggleView} viewType={ViewType.Grid} title={GRID_VIEW} />
+              <TooltipHost id={listTooltip} content={LIST_VIEW}>
+                <ToggleButton iconProps={{ iconName: "BulletedList" }} onClick={this.props.toggleView} viewType={ViewType.List} title={LIST_VIEW} />
+              </TooltipHost>
+              <TooltipHost id={gridTooltip} content={GRID_VIEW}>
+                <ToggleButton iconProps={{ iconName: "GridViewMedium" }} onClick={this.props.toggleView} viewType={ViewType.Grid} title={GRID_VIEW} />
+              </TooltipHost>
               <Sort />
               <Filter />
             </ViewHelperBar>
           </UpperBar>
           <TagList tags={allTags} selectedTags={this.state.selectedTags} allowEdit={false} onClick={this.tagOnClick} toggleStyle={this.tagToggleStyle} />
-          <TemplatesView onClick={this.selectTemplate} selectedTags={this.state.selectedTags} getTemplates={this.props.getTemplates}/>
+          <TemplatesView onClick={this.selectTemplate} selectedTags={this.state.selectedTags} getTemplates={this.props.getTemplates} />
         </InnerCardsContainer>
       </OuterCardsContainer>
     );
