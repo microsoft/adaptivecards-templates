c620872c93b0bcfb37c25a324e2af8f01bdcb75f
Grace Zhang <t-grzhan@microsoft.com>
2020-04-16 11:22:45 -0700

    change owner avatar to persona

--- private-templates-service/client/src/components/Dashboard/TemplateList/OwnerAvatar/styled.tsx
+++ private-templates-service/client/src/components/Dashboard/TemplateList/OwnerAvatar/styled.tsx
@@ -1,24 +1,5 @@
 import styled from 'styled-components';
-import { COLORS } from '../../../../globalStyles';
-
-export const ProfilePic = styled.img`
-  border-radius: 50%;
-`
 
 export const Container = styled.div`
   margin-bottom: 3px;
   `
-
-export const InitialsPic = styled.div`
-  display: flex;
-  flex-direction: column;
-  border-radius: 50%;
-  background-color: ${COLORS.BLUE2};
-  justify-content: center;
-  align-items: center;
-  `
-
-export const Initials = styled.div`
-  text-align: center;
-  color: ${COLORS.WHITE};
-`
