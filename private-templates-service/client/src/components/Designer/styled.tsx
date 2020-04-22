f08ca81a530b23e92540d7975aa08be504f8fa28
guptaharsh25 <me@harshgupta.ca>
2020-04-22 14:35:37 -0700

    typo again

--- private-templates-service/client/src/components/Designer/styled.tsx
+++ private-templates-service/client/src/components/Designer/styled.tsx
@@ -4,7 +4,7 @@ export const OuterDesignerWrapper = styled.main`
   display: flex;
   flex-direction: column;
   height: 100%;
-  overscroll-behaviour: contain;
+  overscroll-behavior: contain;
 `;
 
 export const DesignerWrapper = styled.div`
